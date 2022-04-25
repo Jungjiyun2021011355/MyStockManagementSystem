5958c42e7fde4c800903498e89f150a1b607948b
Jungjiyun2021011355 <101305643+Jungjiyun2021011355@users.noreply.github.com>
2022-04-25 23:41:46 +0900

    Add files via upload

---
+++ Stocklist.java
@@ -0,0 +1,16 @@
+package week5;
+
+public class Stocklist extends Stock{
+	private String object;
+	
+	Stocklist() {
+	}
+	
+	public String getObject() {
+		return object;
+	}
+	public void setObject(String object) {
+		this.object = object;
+	}
+
+}

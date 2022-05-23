f58d30a9c9daa3feb472fdfc615c74304b283732
Jungjiyun2021011355 <101305643+Jungjiyun2021011355@users.noreply.github.com>
2022-05-23 14:13:19 +0900

    Add files via upload

--- ManagementSystem.java
+++ ManagementSystem.java
@@ -1,90 +1,110 @@
 package week8;
-import java.util.*;  
+import java.util.*;
+import java.io.*;
+import log.EventLogger;
+
 import java.text.SimpleDateFormat;
 import java.io.*;
 public class ManagementSystem {
+	Scanner input = new Scanner(System.in);
+	static EventLogger Logger = new EventLogger("log.txt");
+	
 	public static void main(String[] args) {
-		NumberConfinement nc = new NumberConfinement();
 		Scanner input = new Scanner(System.in);
-		Date date = new Date();
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Menu m = new Menu();
-		
-	    try {
-	    	FileOutputStream timefile = new FileOutputStream("logFile.txt", true);
-    	
-		    timefile.write(format.format(date).getBytes());
-	    	
-	        timefile.close();
-	    	
-	    }catch(IOException e) {
-	    	System.out.println(e.getMessage());
-	    }
-	    try {
-	    	FileWriter file = new FileWriter("logFile.txt", true);
-
-
-			int num = input.nextInt();
-			
-			nc.numc(num);
-		    
-			while (num != 6) {
-				System.out.println("*** Stock managementSystem Menu ***");
-				System.out.println("1. open my stock list: ");
-				System.out.println("2. Add my stocks: ");
-			 	System.out.println("3. delete my stocks: ");
-				System.out.println("4. edit my stock list: ");
-				System.out.println("5. view my stocks: ");
-				System.out.println("6. exit");
-				System.out.print("Select one number between 1-6: ");
-				
-				try {
-					int i = input.nextInt();
-					
-				    file.write("메뉴 번호 입력: " + i + "\n");
-
-					nc.numc(i);
-		
-					switch(i) {
-					
-					case 1:
-					    System.out.println("Welcome to visit");
-
-					    break;
-					case 2: 
-						m.add(0);
-
-						break;
-					case 3: 
-						m.delete();
-
-						break;
-					case 4: 
-						m.edit(null, null, null);
-
-						break;
-					case 5: 
-						m.view();
-
-						break;
-					case 6:
-						break;
-					}
-					file.close();
-					
-				}catch (InputMismatchException e) {
-					System.out.println("숫자를 입력해주세요");
-					break;
-				}catch (Exception e) {
-					System.out.println("1부터 6사이의 정수를 입력해주세요");
-					break;
-			    }
-			}
-		}catch (InputMismatchException e) {
-			System.out.println("숫자를 입력해주세요");	
-		}catch (Exception e) {
-			System.out.println("1부터 6사이의 정수를 입력해주세요");
+		Menu m = getObject("m.ser");
+		if(m == null) {
+			m = new Menu();
 		}
+		
+		selectMenu(input, m);
+		putObject(m, "m.ser");
+		
+	}
+    public static void selectMenu(Scanner input, Menu m) {
+    	int num = -1;
+    	while (num != 5) {
+    		try {
+    			showMenu();
+    			num = input.nextInt();
+    			switch(num) {
+				
+				case 1:
+				    System.out.println("Welcome to visit");
+				    break;
+				case 2: 
+					m.add(0);
+					Logger.log("add a stock");
+					break;
+				case 3: 
+					m.delete();
+					Logger.log("delete a stock");
+					break;
+				case 4: 
+					m.edit(null, null, null);
+					Logger.log("edit a stock");
+					break;
+				case 5: 
+					m.view();
+					Logger.log("view a stock");
+					break;
+				default:
+					continue;
+    		    }
+    		}catch(InputMismatchException e) {
+				System.out.print("Select one number between 1-6: ");
+				if (input.hasNext()) {
+					input.next();
+				}
+				num = -1;
+    	    }
+    	}
+    }
+    public static void showMenu() {
+		System.out.println("*** Stock managementSystem Menu ***");
+		System.out.println("1. open my stock list: ");
+		System.out.println("2. Add my stocks: ");
+		System.out.println("3. delete my stocks: ");
+		System.out.println("4. edit my stock list: ");
+		System.out.println("5. view my stocks: ");
+		System.out.println("6. exit");
+		System.out.print("Select one number between 1-6: ");
+    }
+    
+	public static Menu getObject(String filename) {
+		Menu m = null;
+	
+		try {
+			FileInputStream file = new FileInputStream(filename);
+			ObjectInputStream in = new ObjectInputStream(file);
+			
+			m = (Menu) in.readObject();
+			
+			in.close();
+			file.close();
+		} catch(FileNotFoundException e) {
+			return m;
+		} catch(IOException e) {
+			e.printStackTrace();
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return m;
+	}
+	
+	public static void putObject(Menu mn, String filename) {
+		try {
+			FileOutputStream file = new FileOutputStream(filename);
+			ObjectOutputStream out = new ObjectOutputStream(file);
+			
+			out.writeObject(mn);
+			
+			out.close();
+			file.close();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		} 
 	}
 }
 class NumberConfinement{
--- Menu.java
+++ Menu.java
@@ -1,194 +1,86 @@
 package week8;
-import java.util.*;
-import java.io.*;
-import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class Menu extends StockInformation implements Stock{
-
-    int count;
+Scanner input = new Scanner(System.in);
+int count;
 
 	public void add(int count) {
-		Scanner input = new Scanner(System.in);
-		Date date = new Date();
 		
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	    try {
-	    	FileOutputStream file = new FileOutputStream("text.txt", true);
-    	
-		    file.write(format.format(date).getBytes());
-	    	
-	        file.close();
-	    	
-	    }catch(IOException e) {
-	    	System.out.println(e.getMessage());
-	    }
-		try {
-			FileWriter file = new FileWriter("text.txt", true);
-			for(int i = 0; i < 100; i++) {
-				file.write("\t add함수 실행: " + i);
-				
-				System.out.print((i+1) + "번째 주식 이름을 입력하시오");
-				String Name = input.nextLine();
-				super.setName(Name, i);
-				file.write("\t 추가한 이름: " + Name);
-				
-				if(Name.equals(" ")) {
-				    	break;
-				}
-				System.out.println((i+1)+ "번째 주식 아이디를 입력하시오");
-				String ID = input.nextLine();
-				super.setID(ID, i);
-				file.write("\t 추가한 아이디: " + ID);
-				
-				System.out.println((i+1) + "번째 주식 정보를 입력하시오");
-				String Data = input.nextLine();
-			    super.setData(Data, i);
-				file.write("\t 추가한 데이터: " + Data + "\n");
-			    
-			    count++;
-			    this.count = count;
-			}
-			System.out.println("정상적으로 저장되었습니다");
+		for(int i = 0; i < 100; i++) {
 			
-			file.close();
-		}catch(IOException e) {
-			System.out.print(e.getMessage());			
+			System.out.print((i+1) + "번째 주식 이름을 입력하시오");
+			String Name = input.nextLine();
+			super.setName(Name, i);
+			
+			if(Name.equals(" ")) {
+			    	break;
+			}
+			System.out.println((i+1)+ "번째 주식 아이디를 입력하시오");
+			String ID = input.nextLine();
+			super.setID(ID, i);
+			
+			System.out.println((i+1) + "번째 주식 정보를 입력하시오");
+			String Data = input.nextLine();
+		    super.setData(Data, i);
+		    
+		    count++;
+		    this.count = count;
 		}
-		
-		
+	    System.out.println("정상적으로 저장되었습니다");
 	}
 	
-	public void delete() 
-	{
-		Scanner input = new Scanner(System.in);
-		Date date = new Date();
+	public void delete() {
+		ViewConfinement vc = new ViewConfinement();
 		
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	    try {
-	    	FileOutputStream file = new FileOutputStream("text.txt", true);
-    	
-		    file.write(format.format(date).getBytes());
-	    	
-	        file.close();
-	    }catch(IOException e) {
-	    	System.out.println(e.getMessage());
-	    }
 		try {
-		    ViewConfinement vc = new ViewConfinement();
-
 			vc.Count(count);
-		}catch(Exception e) {
+		}catch (Exception e) {
 			System.out.println("저장된 정보가 없습니다.");
 		}
-		try {
+		try { 
+			int i = input.nextInt();
+		
+	    super.setName(null, i);
+		super.setID(null, i);
+		super.setData(null, i);
+		System.out.println("정상적으로 삭제되었습니다");
+		}catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("전체 인덱스 값보다 큰 값으로 접근");
 			
-			FileWriter file = new FileWriter("text.txt", true);
-			try {
-				int i = input.nextInt();
-				file.write("\t Delete함수 실행: " + i);
-				
-			    super.setName(null, i);
-			    file.write("\t 제거한 이름: " + null);
-				super.setID(null, i);
-				file.write("\t 제거한 아이디: " + null);
-				super.setData(null, i);
-				file.write("\t 제거한 데이터: " + null + "\n");
-				System.out.println("정상적으로 삭제되었습니다");
-		    }catch (ArrayIndexOutOfBoundsException e) {
-				System.out.println("전체 인덱스 값보다 큰 값으로 접근");
-			}
-			file.close();
-		}catch(IOException e) {
-			System.out.print(e.getMessage());
-		}	
-
+		}
 	}
 	public void edit(String newName, String newID, String newData) {
-		
-		Date date = new Date();
-		
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	    try {
-	    	FileOutputStream file = new FileOutputStream("text.txt", true);
-    	
-		    file.write(format.format(date).getBytes());
-	    	
-	        file.close();
-	    	
-	    }catch(IOException e) {
-	    	System.out.println(e.getMessage());
-	    }
-	    try {
-			FileWriter file = new FileWriter("text.txt", true);
-			int i = input.nextInt();
-			
-			file.write("\t edit함수 실행: " + i);
+		int i = input.nextInt();
 
-			System.out.println("새로 저장할 이름을 입력하시오");
-			newName = input.nextLine();
-			super.setName(newName, i);
-			file.write("\t 새로 추가한 이름: " + newName);
-			
-			System.out.println("아이디를 입력하시오");
-			newID = input.nextLine();
-			super.setID(newID, i);
-			file.write("\t 새로 추가한 아이디: " + newID);
-			
-			System.out.println("정보를 입력하시오");
-			newData = input.nextLine();
-			super.setData(newData, i);
-			file.write("\t 새로 추가한 데이터: " + newData + "\n");
-			
-			System.out.println("정상적으로 편집되었습니다");
-			file.close();
-
-	    }catch(IOException e) {
-	    	System.out.print(e.getMessage());
-	    }
+		System.out.println("새로 저장할 이름을 입력하시오");
+		newName = input.nextLine();
+		super.setName(newName, i);
 		
+		System.out.println("아이디를 입력하시오");
+		newID = input.nextLine();
+		super.setID(newID, i);
+		
+		System.out.println("정보를 입력하시오");
+		newData = input.nextLine();
+		super.setData(newData, i);
+		
+		System.out.println("정상적으로 편집되었습니다");
 	}
 	public void view() {
-		Date date = new Date();
-		
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	    try {
-	    	FileOutputStream file = new FileOutputStream("text.txt", true);
-    	
-		    file.write(format.format(date).getBytes());
-	    	
-	        file.close();
-	    }catch(IOException e) {
-	        System.out.println(e.getMessage());	
-	    }	
+	    ViewConfinement vc = new ViewConfinement();
 		try {
-			FileWriter file = new FileWriter("text.txt", true);
-			try {
-			
-			    ViewConfinement vc = new ViewConfinement();
-
-				vc.Count(count);
-				for(int i = 0; i < count; i++) {
-					file.write("\t view함수 실행: " + i);
-					System.out.println("주식 이름: " + super.getName(i));
-					file.write("\t 출력한 주식이름: " + super.getName(i));
-					
-					System.out.println("주식 아이디: " + super.getID(i));
-					file.write("\t 출력한 주식아이디: " + super.getID(i));
-					
-					System.out.println("주식 데이터: " + super.getData(i));
-					file.write("\t 출력한 주식데이터: " + super.getData(i) + "\n");
-
-					System.out.println("-------------------------");
-				}
-			}catch (Exception e) {
-				System.out.println("저장된 정보가 없습니다.");
-			}
-			System.out.println("정상적으로 출력되었습니다");
-		    file.close();
-		}catch(IOException e) {
-			System.out.print(e.getMessage());
+			vc.Count(count);
+		}catch (Exception e) {
+			System.out.println("저장된 정보가 없습니다.");
 		}
-
+		for(int i = 0; i < count; i++) {
+			System.out.println("주식 이름: " + super.getName(i));
+			System.out.println("주식 아이디: " + super.getID(i));
+			System.out.println("주식 데이터: " + super.getData(i));
+			System.out.println("-------------------------");
+		}
+		System.out.println("정상적으로 출력되었습니다");
 	}
 }
 
